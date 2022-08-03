782c95246189196237c963c2aa18f582a27e52da
osmanjosue <osmanjosue@gmail.com>
2022-08-03 16:26:42 -0600

    seccion 38 final practica decorador @output comunicacion componente hijo a padre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,8 @@ export class AppComponent {
     new Persona('Laura', 'Juarez'),
     new Persona('Karla', 'Lara')
   ];
-  nombreInput:string = '';
-  apellidoInput:string = '';
-
-  agregarPersona(){
-    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
-    this.personas.push( persona1 );
+  personaAgregada(persona: Persona){
+    this.personas.push(persona);
   }
+  
 }
