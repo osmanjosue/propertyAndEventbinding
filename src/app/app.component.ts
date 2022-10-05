76b11e18ff62e770d7820f50c78228cfe9a395fe
osmanjosue <osmanjosue@gmail.com>
2022-10-05 09:04:49 -0600

    55 clase final-declara las variables inyecta los servicios crea los 4 metodos crea los @inputs en cabecerocomponent.ts envia los inputs desde componente padre appcomponent.html mediante <app-cabecero>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EgresoServicio } from './egreso/egreso.servicio';
+import { Ingreso } from './ingreso/ingreso.model';
+import { IngresoServicio } from './ingreso/ingreso.servicio';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'presupuesto-app';
+
+  ingresos:Ingreso[]=[];
+  egresos:Ingreso[]=[];
+
+  constructor(private ingresoServicio:IngresoServicio, 
+              private egresoServicio:EgresoServicio){
+                this.ingresos=ingresoServicio.ingresos;
+                this.egresos=egresoServicio.egresos;
+              }
+
+getIngresoTotal(){
+  let ingresoTotal:number=0;
+  this.ingresos.forEach(ingreso=>{
+    ingresoTotal += ingreso.valor;
+  }
+  );
+  return ingresoTotal;
+}
+
+getEgresoTotal(){
+  let egresoTotal:number=0;
+  this.egresos.forEach(egreso=>{
+    egresoTotal += egreso.valor;
+  }
+  );
+  return egresoTotal;              
+}
+
+getPorcentajeTotal(){
+  return this.getEgresoTotal()/this.getIngresoTotal();
+}
+getPresupuestoTotal(){
+  this.getIngresoTotal() - this.getEgresoTotal();
+}
+
+ 
 }
