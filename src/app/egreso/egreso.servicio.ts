4a0f572ba5c21a6ec742be721a6dd93fbc850be2
osmanjosue <osmanjosue@gmail.com>
2022-10-04 21:26:34 -0600

    53 clase final definicion de los servicios y modelos de egreso e ingreso

---
+++ src/app/egreso/egreso.servicio.ts
@@ -0,0 +1,9 @@
+import { Egreso } from "./egreso.model";
+
+export class EgresoServicio{
+    egresos:Egreso[]=[
+        new Egreso("renta depto", 900),
+        new Egreso("ropa", 200)
+
+    ];
+}
---
+++ src/app/ingreso/ingreso.servicio.ts
@@ -0,0 +1,10 @@
+import { Ingreso } from "./ingreso.model";
+
+export class IngresoServicio{
+
+    ingresos:Ingreso[]=[
+        new Ingreso("salario", 4000),
+        new Ingreso("venta choche", 500)
+    ];
+
+}
