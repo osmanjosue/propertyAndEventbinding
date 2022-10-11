8352733d5fa602b9c9cdb23cea04d3cf104a2891
osmanjosue <osmanjosue@gmail.com>
2022-10-11 17:48:09 -0600

    64 clase final-configuracion del app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { FormularioComponent } from './personas/formulario/formulario.component';
+import { PersonasComponent } from './personas/personas.component';
 
 
+
+const routes:Routes=[
+  {path: '', component:PersonasComponent},
+  {path: 'personas', component:PersonasComponent},
+  {path: 'personas/agregar', component:FormularioComponent},
+  {path: 'personas/:id', component:FormularioComponent},
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(
+    routes
+  )
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
