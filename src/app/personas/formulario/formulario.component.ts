b34d0af5a30e2c2db032affe77f82d16b684d34b
osmanjosue <osmanjosue@gmail.com>
2022-10-11 23:55:03 -0600

    68 clase final-query params, sepa putas para que es

--- src/app/personas/formulario/formulario.component.ts
+++ src/app/personas/formulario/formulario.component.ts
@@ -12,6 +12,7 @@ export class FormularioComponent {
   nombreInput: string;
   apellidoInput: string;
   index: number;
+  modoEdicion: number;
 
   constructor(
     private personasService: PersonasService,
@@ -25,7 +26,8 @@ export class FormularioComponent {
 
   ngOnInit() {
     this.index=this.route.snapshot.params['id'];
-    if(this.index){
+this.modoEdicion=+this.route.snapshot.queryParams['modoEdicion'];
+    if(this.modoEdicion!=null && this.modoEdicion===1){
       let persona:Persona=this.personasService.encontrarPersona(this.index);
       this.nombreInput=persona.nombre;
       this.apellidoInput=persona.apellido;
@@ -34,7 +36,7 @@ export class FormularioComponent {
 
   onGuardarPersona() {
     let persona1 = new Persona(this.nombreInput, this.apellidoInput);
-    if(this.index){
+    if(this.modoEdicion!=null && this.modoEdicion===1){
 
       this.personasService.modificarPersona(this.index, persona1);
 
