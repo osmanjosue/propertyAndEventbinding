64e0ddfb0ce316cd97f07df5cdf41601e3190297
osmanjosue <osmanjosue@gmail.com>
2022-10-11 23:18:05 -0600

    67 clase final-como eliminar usuario, NO LE ENTENDI MUCHO

--- src/app/persona.service.ts
+++ src/app/persona.service.ts
@@ -1,32 +1,34 @@
-import { EventEmitter, Injectable } from "@angular/core";
-import { LoggingService } from "./LoggingService.service";
-import { Persona } from "./persona.model";
-
+import { EventEmitter, Injectable } from '@angular/core';
+import { LoggingService } from './LoggingService.service';
+import { Persona } from './persona.model';
 
 @Injectable()
-export class PersonasService{
-    personas: Persona[] = [
-        new Persona('Juan','Perez'), 
-        new Persona('Laura', 'Juarez')        
-      ];
+export class PersonasService {
+  personas: Persona[] = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Laura', 'Juarez'),
+  ];
 
-      saludar=new EventEmitter<number>();
+  saludar = new EventEmitter<number>();
 
-      constructor(private logginService:LoggingService){}
+  constructor(private logginService: LoggingService) {}
 
-      agregarPersona(persona: Persona){
-        this.logginService.enviaMensajeAConsola("agregamos persona: " + persona.nombre);
-        this.personas.push(persona);
-      }
-      encontrarPersona(index:number){
-        let persona:Persona=this.personas[index];
-        return persona;
-      }
-      modificarPersona(index:number, persona:Persona){
-
-        let persona1=this.personas[index];
-        persona1.nombre=persona.nombre;
-        persona1.apellido=persona.apellido;
-        
-      }
+  agregarPersona(persona: Persona) {
+    this.logginService.enviaMensajeAConsola(
+      'agregamos persona: ' + persona.nombre
+    );
+    this.personas.push(persona);
+  }
+  encontrarPersona(index: number) {
+    let persona: Persona = this.personas[index];
+    return persona;
+  }
+  modificarPersona(index: number, persona: Persona) {
+    let persona1 = this.personas[index];
+    persona1.nombre = persona.nombre;
+    persona1.apellido = persona.apellido;
+  }
+  eliminarPersona(index:number){
+    this.personas.splice(index,1);
+  }
 }
